c13219386b9fa7df3d20fe869a0ac867c919c42a
Paul Fomichov <pavel.fomichov@intellecteu.com>
2018-12-20 07:19:30 +0200

    init

---
+++ app/src/main/java/com/paulilves/templateapp/AppModule.kt
@@ -0,0 +1,9 @@
+package com.paulilves.templateapp
+
+import org.koin.dsl.module.module
+
+/**
+ * Koin dependency injection config class - add dependencies inside module closure
+ */
+val appModule = module {
+}
---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.10"))
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+//tasks.register("clean", Delete::class) {
+//    delete(rootProject.buildDir)
+//}
